cc7d9b78dd777db1b4dedc0f6773c6ee158b59bc
Joe Beeson <jbeeson@gmail.com>
2010-03-12 15:17:22 -0700

    config/schema/schema.php  - Added  - Schema for our plugin

---
+++ config/schema/schema.php
@@ -0,0 +1,50 @@
+<?php 
+class schemaSchema extends CakeSchema {
+	var $name = 'schema';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	var $message_recipient_attachments = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
+		'message_recipient_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36),
+		'file' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 150),
+		'type' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100),
+		'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100),
+		'indexes' => array(),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+	var $message_recipient_variables = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
+		'message_recipient_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36),
+		'key' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 150),
+		'value' => array('type' => 'text', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+	var $message_recipients = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
+		'message_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36),
+		'recipient' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 150),
+		'tries' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
+		'processed' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
+		'modified' => array('type' => 'timestamp', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'timestamp', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+	var $messages = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
+		'layout' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 150),
+		'template' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 150),
+		'from' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 150),
+		'subject' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 150),
+		'created' => array('type' => 'timestamp', 'null' => false, 'default' => 'CURRENT_TIMESTAMP'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+}
