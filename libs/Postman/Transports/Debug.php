e9f32f85584c36952f2391d821235af3092260f3
Joe Beeson <jbeeson@gmail.com>
2010-10-19 17:20:30 -0600

    Adding some default settings and an _initailize method to confirm the folder exists and is writable.

--- libs/Postman/Transports/Debug.php
+++ libs/Postman/Transports/Debug.php
@@ -14,6 +14,17 @@
 	class Debug extends \Postman\Library\Transport {
 
 		/**
+		 * Default settings. Any configuration passed to `setSettings` will
+		 * be merged into this member variable.
+		 *
+		 * @var array
+		 * @access protected
+		 */
+		protected $_settings = array(
+			'folder' => TMP
+		);
+
+		/**
 		 * Sends the given `\Postman\Library\Email` object. Returns boolean to
 		 * indicate success of the operation.
 		 *
@@ -22,7 +33,33 @@
 		 * @access public
 		 */
 		public function send(\Postman\Library\Email $email) {
+
 			pr($email);
+
+		}
+
+		/**
+		 * Initialization method, called after construction.
+		 *
+		 * @return null
+		 * @access protected
+		 */
+		protected function _initialize() {
+
+			// Ensure that the folder to write to exists
+			if (!file_exists($this->_getSetting('folder'))) {
+				throw new \InvalidArgumentException(
+					'Debug::$_settings[\'folder\'] must be a valid, existing folder'
+				);
+			}
+
+			// Ensure that the folder to write to is writable
+			if (!is_writable($this->_getSetting('folder'))) {
+				throw new \InvalidArgumentException(
+					'Debug::$_settings[\'folder\'] must be writable'
+				);
+			}
+
 		}
 
 	}
