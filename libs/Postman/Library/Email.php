f582ce8030d02b0d00a1beee99658b13d8227a25
Joe Beeson <jbeeson@gmail.com>
2011-03-08 13:14:04 -0700

    Making it so the various methods accept either a string or their corresponding object, just in case you happen to have that around.

--- libs/Postman/Library/Email.php
+++ libs/Postman/Library/Email.php
@@ -91,16 +91,23 @@
 		}
 
 		/**
-		 * Creates a new `\Postman\Library\Email\Address` object and sets its
-		 * address with the given parameter then assigns the object to our
-		 * `$_replyTo` member variable.
+		 * Sets our `_replyTo` address. Accepts a string or an `Address`
+		 * object.
 		 *
-		 * @param string $address
+		 * @param mixed $address
 		 * @return \Postman\Library\Email\Address
 		 * @access public
 		 */
 		public function setReplyTo($address) {
-			$address = new \Postman\Library\Email\Address($address);
+			if (is_string($address)) {
+				$address = new \Postman\Library\Email\Address($address);
+			} elseif (!is_a($address, '\Postman\Library\Email\Address')) {
+				throw new \InvalidArgumentException(
+					'Email::setReplyTo expects a string or valid Address object.'	
+				);
+			}
+
+			// Set the `_replyTo` and return the object.
 			$this->_replyTo = $address;
 			return $address;
 		}
@@ -145,18 +152,23 @@
 		}
 
 		/**
-		 * Creates a new `\Postman\Library\Email\Address` object and sets its
-		 * address with the given parameter then assigns the object to our
-		 * `$_from` member variable.
+		 * Sets our `_from` address. Accepts a string or an `Address`
+		 * object.
 		 *
-		 * Returns the newly created `Address` object.
-		 *
-		 * @param string $from
+		 * @param mixed $from
 		 * @return \Postman\Library\Email\Address
 		 * @access public
 		 */
 		public function setFrom($from) {
-			$from = new \Postman\Library\Email\Address($from);
+			if (is_string($from)) {
+				$from = new \Postman\Library\Email\Address($from);
+			} elseif (!is_a($from, '\Postman\Library\Email\Address')) {
+				throw new \InvalidArgumentException(
+					'Email::setFrom expects a string or valid Address object.'
+				);
+			}
+
+			// Set the `_from_ and return the object.
 			$this->_from = $from;
 			return $from;
 		}
@@ -171,10 +183,15 @@
 		 * @access public
 		 */
 		public function addRecipient($recipient, $type = 'to') {
-			$recipient = new \Postman\Library\Email\Address\Recipient(
-				$recipient,
-				$type
-			);
+			if (is_string($recipient)) {
+				$recipient = new \Postman\Library\Email\Address\Recipient($recipient, $type);
+			} elseif (!is_a($recipient, '\Postman\Library\Email\Address\Recipient')) {
+				throw new \InvalidArgumentException(
+					'Email::addRecipient expects a string or valid Address object.'
+				);
+			}
+
+			// Add the recipient and return the object.
 			$this->_recipients[] = $recipient;
 			return $recipient;
 		}
@@ -189,10 +206,18 @@
 		 * @access public
 		 */
 		public function addAttachment($file, $name = '') {
-			$attachment = new \Postman\Library\Email\Attachment(
-				$file,
-				$name
-			);
+			if (is_string($file)) {
+				$attachment = new \Postman\Library\Email\Attachment(
+					$file,
+					$name
+				);
+			} elseif (!is_a($file, '\Postman\Library\Email\Attachment')) {
+				throw new \InvalidArgumentException(
+					'Email::addAttachment expects a string or valid Attachment object.'
+				);
+			}
+
+			// Add the attachment and return the object.
 			$this->_attachments[] = $attachment;
 			return $attachment;
 		}
