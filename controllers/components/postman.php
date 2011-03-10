dd5b167cf6c3dea1eecd39292728b407ba5ffa34
Joe Beeson <jbeeson@gmail.com>
2011-03-10 15:49:08 -0700

    Changed the way settings and transports are defined.

--- controllers/components/postman.php
+++ controllers/components/postman.php
@@ -36,6 +36,15 @@
 		protected $_settings = array();
 
 		/**
+		 * Holds any initialized transports keyed off their
+		 * name for easier reuse.
+		 *
+		 * @var array
+		 * @access protected
+		 */
+		protected $_transports = array();
+
+		/**
 		 * Triggered before the controller's `beforeFilter` method but after the
 		 * models have been constructed.
 		 *
@@ -46,6 +55,8 @@
 		 * @see http://book.cakephp.org/view/1617/Component-callbacks
 		 */
 		public function initialize(Controller $controller, $settings = array()) {
+
+			// Go get our Postman object.
 			App::import(
 				'Libs',
 				'Postman.Postman',
@@ -53,16 +64,23 @@
 					'file' => 'Postman.php'
 				)
 			);
+
+			// Setup some default settings.
+			$this->_getPostman();
 			$this->_settings = $settings;
-			if (count($this->_settings) > 0) {
-				$transport = array_shift(array_keys($this->_settings));
-				if (is_array($this->_settings[$transport])) {
-					$settings = $this->_settings[$transport];
-				} else {
-					$settings = array();
+			$transport = null;
+
+			// Loop over and create our transports.
+			foreach ($this->_settings as $transport=>$settings) {
+				if (is_numeric($transport) and is_string($settings)) {
+					$transport = $settings;
+					$settings  = array();
 				}
-				$this->setTransport($transport, $settings);
+				$this->addTransport($transport, $settings);
 			}
+
+			// Be sure to attempt and set a transport.
+			$this->setTransport($transport);
 		}
 
 		/**
@@ -89,10 +107,16 @@
 		 * `Postman` object. Returns boolean to indicate success.
 		 *
 		 * @param \Postman\Library\Email $email
+		 * @param mixed $transport
 		 * @return boolean
 		 * @access public
 		 */
-		public function send(\Postman\Library\Email $email) {
+		public function send(\Postman\Library\Email $email, $transport = null) {
+			if (!is_null($transport)) {
+				$this->setTransport($transport);
+			}
+
+			// Tell the object to send it off.
 			return $this->_getPostman()->send($email);
 		}
 
@@ -105,8 +129,50 @@
 		 * @return null
 		 * @access public
 		 */
-		public function setTransport($transport, $settings = array()) {
-			$this->_getPostman()->setTransport($transport, $settings);
+		public function setTransport($transport) {
+			$this->addTransport($transport);
+			$this->_getPostman()->setTransport(
+				$this->getTransport($transport)
+			);
+		}
+
+		/**
+		 * Adds the given transport to our `$_transports` member variable.
+		 *
+		 * @param mixed $transport
+		 * @param array $settings
+		 * @return void
+		 * @access public
+		 */
+		public function addTransport($transport, $settings = array()) {
+			if (is_object($transport)) {
+				$this->_transports[get_class($transport)] = $transport;
+			} else {
+				$class = '\Postman\Transports\\' . $transport;
+				if (!isset($this->_transports[$class]) or !empty($settings)) {
+					$this->_transports[$class] = new $class($settings);
+				}
+			}
+
+			// Assume they want this as the primary
+			$this->setTransport($transport);
+		}
+
+		/**
+		 * Returns the requested transport, by string.
+		 *
+		 * @param string $transport
+		 * @return mixed
+		 * @access public
+		 */
+		public function getTransport($transport) {
+			if (!array_key_exists($transport, $this->_transports)) {
+				$transport = '\Postman\Transports\\' . $transport;
+			}
+			if (array_key_exists($transport, $this->_transports)) {
+				return $this->_transports[$transport];
+			}
+			return null;
 		}
 
 		/**
