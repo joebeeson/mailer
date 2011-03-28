de4c27935211693150f35dad0c7bc599bc08605b
Joe Beeson <jbeeson@gmail.com>
2011-03-28 11:17:19 -0600

    Checking if the class already exists.

--- libs/Postman/bootstrap.php
+++ libs/Postman/bootstrap.php
@@ -4,7 +4,9 @@
 	 * Setup our autoloading class to handle the automatic inclusion of requested
 	 * classes.
 	 */
-	App::import('Lib', 'Mailer.loader');
+	if (!class_exists('SplClassLoader')) {
+		App::import('Lib', 'Mailer.loader');
+	}
 	$classLoader = new SplClassLoader(
 		'Postman',
 		dirname(dirname(__FILE__))
